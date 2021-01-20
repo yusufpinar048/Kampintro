95bc87dda25d1ccc2a336ebfd334562ae9e14f63
Yusuf pinar <yusufpinar048@gmail.com>
2021-01-20 23:50:25 +0300

    deneme

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyList<string> isimler = new MyList<string>();
+
+            isimler.Add("Engin");
+            isimler.Add("Engin");
+            isimler.Add("Engin");
+
+            isimler.Add("Engin");
+
+            foreach (var item in isimler)
+            {
+                Console.WriteLine(isimler);
+            }
+
+
+        }
+    }
+}
